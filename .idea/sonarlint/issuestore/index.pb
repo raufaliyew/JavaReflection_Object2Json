5ba358fb4ac36c13b66c153ece600877c5bf29be
Aqil <aqilaghamirzayev@gmail.com>
2022-10-23 19:37:59 +0400

    Add create objects without new keyword

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -8,3 +8,15 @@ a
 1java-performance-tuning-memory-management/pom.xml,8\4\84d69ff08522376ee6edc9681296c65c74375399
 9
 	README.MD,9\6\968e80af9562e09cebe594968b130076952eafe1
+Ö
+Ujava-performance-tuning-memory-management/src/main/java/performanceexample1/Main.java,0\e\0eeac51d5411eecb56dab360efbc208657733253
+ç
+]java-performance-tuning-memory-management/src/main/java/performanceexample1/PrimeNumbers.java,a\5\a5e8f658f35b0f55fe27375180cb6e7a4445d69c
+Ö
+Ujava-performance-tuning-memory-management/src/main/java/performanceexample2/Main.java,9\5\954f305ff167878d89e79bcf422ec38a0355a621
+G
+java-reflection/pom.xml,6\e\6e70690be9466c19007b203d38133f14e3284fbd
+ç
+]java-performance-tuning-memory-management/src/main/java/performanceexample2/PrimeNumbers.java,a\7\a7fec6068c1b718638451a915084d68dbb2e9139
+b
+2java-reflection/src/main/java/jenkov/com/Test.java,a\2\a2ea01746be12fda70432bb18e4b4b02e5284245
---
+++ java-reflection/src/main/java/udemy_master_class/section_2/Main.java
@@ -0,0 +1,103 @@
+package udemy_master_class.section_2;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Address address = createInstanceWithArguments(Address.class, "Narimanov", 10);
+        Person person = createInstanceWithArguments(Person.class, address, "Ali", 20);
+
+        System.out.println(person);
+    }
+
+    public static <T> T createInstanceWithArguments(Class<T> clazz, Object... args)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterTypes().length == args.length) {
+                return (T) constructor.newInstance(args);
+            }
+        }
+
+        System.out.println("An appropriate constructor was not found");
+        return null;
+    }
+
+    public static void printConstructData(Class<?> clazz) {
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        System.out.println(String.format("class %s has %d declared constructors",
+                clazz.getSimpleName(), constructors.length));
+
+        for (Constructor<?> constructor : constructors) {
+            Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+            List<String> parameterTypeNames = Arrays.stream(parameterTypes)
+                    .map(Class::getSimpleName)
+                    .collect(Collectors.toList());
+
+            System.out.println(parameterTypeNames);
+        }
+
+    }
+
+
+    public static class Person {
+        private final Address address;
+        private final String name;
+        private final int age;
+
+
+        public Person() {
+            this.address = null;
+            this.name = "anonymous";
+            this.age = 0;
+        }
+
+        public Person(String name) {
+            this.address = null;
+            this.name = name;
+            this.age = 0;
+        }
+
+        public Person(String name, int age) {
+            this.address = null;
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person(Address address, String name, int age) {
+            this.address = address;
+            this.name = name;
+            this.age = age;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("Person [address: %s, name: %s, age: %s]", address, name, age);
+        }
+    }
+
+    public static class Address {
+        private String street;
+        private int number;
+
+        public Address(String street, int number) {
+            this.street = street;
+            this.number = number;
+        }
+
+
+        @Override
+        public String toString() {
+            return String.format("Address [street: %s, number: %s]", street, number);
+        }
+    }
+}
